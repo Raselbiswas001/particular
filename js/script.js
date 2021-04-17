9da1fc8e394af956915d7810955399c8baa7e21f
RaselBiswas <77580922+Raselbiswas001@users.noreply.github.com>
2021-04-17 16:21:53 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(document).ready( function(){
+    
+ 
+ //load more
+     $('.loadMore').loadMoreResults({
+    button: {
+      'class': 'btn-load-more',
+      'text': 'Load More'
+    },
+    displayedItems: 5,
+    showItems: 2
+    });
+
+         
+
+
+
+
+
+});
+
+  
